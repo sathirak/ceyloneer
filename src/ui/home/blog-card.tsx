5267f765c0ef2be35b7ef7fb7518c6c8e6001621
sathira/kulathunga <145209193+sathirak@users.noreply.github.com>
2024-03-26 04:14:52 +0000

    Made better CSS and UI Components

--- src/ui/home/blog-card.tsx
+++ src/ui/home/blog-card.tsx
@@ -11,13 +11,13 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ title, excerpt, link, imageUrl, altText }) => {
 	return (
-		<div className="bg-white shadow-md rounded-md rounded-2xl">
+		<div className="bg-white shadow-md rounded-2xl hover:outline outline-cyan-700 outline-offset-2 ">
             <div className="relative w-full h-60  ">
 			    <Image src={imageUrl} alt={altText} className=" object-cover rounded-t-2xl" fill/>
             </div>
-			<h2 className="text-xl text-indigo-900 font-bold mb-2 p-4">{title}</h2>
+			<h2 className="text-xl text-cyan-950 font-bold mb-2 p-4">{title}</h2>
 			<p className="text-gray-600 p-4">{excerpt}</p>
-			<a href={link} className="text-indigo-900 hover:underline font-bold mb-4 p-6">
+			<a href={link} className="text-cyan-950 hover:underline font-bold m-6 mb-3 mt-3 block">
 				Read more
 			</a>
 		</div>
