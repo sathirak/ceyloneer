9d0628ce03d45bf902a2cb96ed9c74f80d564238
sathira/kulathunga <145209193+sathirak@users.noreply.github.com>
2024-03-29 05:12:19 +0000

    Blogcard Skeleton update and manually slowed down data fetch

--- src/ui/home/blog-card-wrapper.tsx
+++ src/ui/home/blog-card-wrapper.tsx
@@ -4,27 +4,35 @@ import { BlogCardSkele } from "./blog-card";
 import { Suspense } from "react";
 
 export default async function BlogCardWrapper() {
-    try {
-        const articles = await fetch_front_articles();
+	try {
+		const articles = await fetch_front_articles();
 
-        return (
-            <>
-                <Suspense fallback={<BlogCardSkele />}>
-                    {articles?.map((art, index) => (
-                        <BlogCard
-                            key={index}
-                            imageUrl={art.imageurl}
-                            altText={art.alttext}
-                            title={art.title}
-                            excerpt={art.excerpt}
-                            link={art.link}
-                        />
-                    ))}
-                </Suspense>
-            </>
-        );
-    } catch (error) {
-        console.error('Failed to fetch articles:', error);
-        return <div>Error fetching articles</div>;
-    }
+		return (
+			<>
+				
+					{articles?.map((art, index) => (
+						<BlogCard key={index} imageUrl={art.imageurl} altText={art.alttext} title={art.title} excerpt={art.excerpt} link={art.link} />
+					))}
+				
+			</>
+		);
+	} catch (error) {
+		console.error("Failed to fetch articles:", error);
+		return <div>Error fetching articles</div>;
+	}
 }
+
+export const BlogCardsSkele = () => {
+	return (
+		<>
+			<BlogCardSkele />
+			<BlogCardSkele />
+			<BlogCardSkele />
+			<BlogCardSkele />
+			<BlogCardSkele />
+			<BlogCardSkele />
+			<BlogCardSkele />
+			<BlogCardSkele />
+		</>
+	);
+};
--- src/ui/home/blog-card.tsx
+++ src/ui/home/blog-card.tsx
@@ -25,20 +25,19 @@ const BlogCard: React.FC<BlogCardProps> = ({ title, excerpt, link, imageUrl, alt
             
 
             <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full">
-                <a href="#">
+
                     <Image loading="lazy" src={imageUrl} alt={altText} width={300} height={200} className="rounded-t-lg w-[100%] h-auto" style={{maxWidth: '100%', height: 'auto'}} />
-                </a>
                 <div className="p-5">
-                    <a href="#">
+
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-                    </a>
+
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{excerpt}</p>
-                    <a href={link} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <Link href={link} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Read more
                         <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
-                    </a>
+                    </Link>
                 </div>
             </div>
 
@@ -49,12 +48,15 @@ const BlogCard: React.FC<BlogCardProps> = ({ title, excerpt, link, imageUrl, alt
 
 export const BlogCardSkele = () => {
     return (
-        <div className="bg-white shadow-md rounded-xl hover:outline outline-cyan-700 outline-offset-2 animate-pulse">
-            <div className="relative w-full h-60 bg-gray-200 rounded-t-xl"></div>
-            <div className="p-4">
-                <div className="h-6 bg-gray-300 w-3/4 mb-2"></div>
-                <div className="h-4 bg-gray-300 w-1/2 mb-3"></div>
-                <div className="h-4 bg-gray-300 w-3/4"></div>
+        <div className="max-w-sm bg-gray-200 border border-gray-200 rounded-lg shadow w-80">
+            <div className="animate-pulse">
+                <div className="bg-gray-400 rounded-t-lg w-full h-[200px]"></div>
+                <div className="p-5">
+                    <div className="bg-gray-400 h-6 mb-4 rounded"></div>
+                    <div className="bg-gray-400 h-4 mb-3 rounded"></div>
+                    <div className="bg-gray-400 h-4 mb-3 rounded w-3/4"></div>
+                    <div className="bg-gray-400 h-4 w-1/2 rounded"></div>
+                </div>
             </div>
         </div>
     );
